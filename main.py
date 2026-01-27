fb01d9c4bdecb4704086dc750b060ffd135d67e5
tewicklund <tewicklund@ucdavis.edu>
2026-01-27 12:26:44 -0800

    Made initial adjustments to convert from serial to LAN

--- main.py
+++ main.py
@@ -3,44 +3,65 @@
 #extends the capabilities of the built in logging system
 
 #imports
-import serial
 import time
-import requests as req
+import socket
 
 #user variables
-#PA_PORTS=['/dev/ttyUSB0','/dev/ttyUSB1']
-#PA_PORTS=['COM8']
-PA_PORTS=['/dev/ttyUSB0']
-trialog_url="http://192.168.99.49:15110/api/broker/battery"
+xitron_ip_list = ["192.168.0.83"]
+xitron_port_list = [10733]
 
 
 #function for setting up serial comms
-def serial_init(PORTS):
-    serial_objects=[]
-    for pa_port in PORTS:
-        serial_object=serial.Serial(port=pa_port, baudrate=115200,timeout=3,dsrdtr=True)
-        serial_objects.append(serial_object)
-    return serial_objects
+# def serial_init(PORTS):
+#     serial_objects=[]
+#     for pa_port in PORTS:
+#         serial_object=serial.Serial(port=pa_port, baudrate=115200,timeout=3,dsrdtr=True)
+#         serial_objects.append(serial_object)
+#     return serial_objects
+
+def send_scpi_command(ip_address, port, command):
+    # Create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        # Connect to the specified IP address and port
+        s.connect((ip_address, port))
+        s.settimeout(10)
+        
+        # Send the SCPI command
+        s.sendall(command.encode())
+        
+        # Receive the response (assuming it's not too large)
+        response = s.recv(1024).decode()
+        
+        # Print and return the response
+        #print("Response:", response)
+        return response
+        
+    finally:
+        # Close the socket
+        s.close()
 
 #function for getting pa data via serial comms
-def log_pa_response(pa,measurement_number,query_string,log_file):
+def log_pa_response(xitron_ip,xitron_port,measurement_number,query_string,log_file):
     #get measurement date
-    pa.write("DATE?\n".encode("ascii"))
-    date_string=pa.readline().decode().removesuffix('\r\n')
+    #pa.write("DATE?\n".encode("ascii"))
+    date_string = send_scpi_command(xitron_ip, xitron_port, "DATE?\n")
+    #date_string=pa.readline().decode().removesuffix('\r\n')
 
     #get measurement time
-    pa.write("TIME?\n".encode("ascii"))
-    time_string=pa.readline().decode().removesuffix('\r\n')
+    #pa.write("TIME?\n".encode("ascii"))
+    time_string = send_scpi_command(xitron_ip,xitron_port,"TIME?\n")
+    #time_string=pa.readline().decode().removesuffix('\r\n')
 
     #get measurement values
-    pa.write(query_string.encode("ascii"))
-    measurement_string=pa.readline().decode().removesuffix('\r\n')
-    response=req.get(url=trialog_url)
-    soc_string=str(response.json()['soc'])
+    #pa.write(query_string.encode("ascii"))
+    measurement_string = send_scpi_command(xitron_ip, xitron_port, query_string)
+    #measurement_string=pa.readline().decode().removesuffix('\r\n')
 
 
     #build log string
-    log_string=str(measurement_number)+','+date_string+','+time_string+','+measurement_string+','+soc_string
+    log_string=str(measurement_number)+','+date_string+','+time_string+','+measurement_string
     f=open(log_file,"a")
     f.write(log_string)
     f.close()
@@ -51,7 +72,7 @@ delay_int=int(input("Enter delay period before test start in seconds: "))
 log_file_name=input("Enter name of log file: ")
 
 #build list of power analyzers as pyserial objects
-power_analyzers=serial_init(PA_PORTS)
+#power_analyzers=serial_init(PA_PORTS)
 
 #get query string from file
 f=open('query_string.txt','r')
@@ -62,8 +83,7 @@ print(f'Found query string {query_string}')
 #set up log file
 f=open(log_file_name,'w')
 f.write("SAMPLE NUM,DATE,TIME,")
-f.write(query_string.removeprefix('READ?,').removesuffix('\n'))
-f.write(',SoC\n')
+f.write(query_string.removeprefix('READ?,'))
 f.close()
 
 #wait for delay time
@@ -78,9 +98,9 @@ for x in range(duration_int):
     start_time=time.time()
 
     #write data to log
-    for analyzer_num in range(len(power_analyzers)):
-        log_pa_response(power_analyzers[analyzer_num],x+1,query_string,log_file_name)
-        print(f'logged point {x+1} out of {duration_int} from analyzer at {PA_PORTS[analyzer_num]}')
+    for analyzer_num in range(len(xitron_ip_list)):
+        log_pa_response(xitron_ip_list[analyzer_num],x+1,query_string,log_file_name)
+        print(f'logged point {x+1} out of {duration_int} from analyzer at {xitron_ip_list[analyzer_num]}')
     
     #write endline to log (so all PA data from the same time ends up in the same row)
     f=open(log_file_name,'a')
